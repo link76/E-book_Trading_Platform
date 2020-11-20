e08e23fcf2307dba2635238b385f5fa5bfb5e444
link76 <43428727+link76@users.noreply.github.com>
2020-11-21 01:15:35 +0800

    Add files via upload
    APIControllers

---
+++ Code/Controllers/Controllers/AdminController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using E_Book_Trading_Platform.Models;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace E_Book_Trading_Platform.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    //public class OrderController : ControllerBase
+    public class AdminController : ControllerBase
+    {
+
+        private readonly Models.PlatformContext _context;
+
+        public AdminController(Models.PlatformContext context)
+        {
+            _context = context;
+        }
+
+        // GET:
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Admins.ToList());
+        }
+        //GET: 
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var _order = _context.Admins.FirstOrDefault(r => r.Id == id);
+            if (_order == null)
+                return NotFound();
+            return Ok(_order);
+        }
+        // POST api/<OrderController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Admin admin)
+        {
+            _context.Add(admin);
+            _context.SaveChanges();
+            return Created($"api/Order/{admin.Id}", admin);
+        }
+
+            // PUT api/<OrderController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Admin admin)
+        {
+            var _order = _context.Admins.FirstOrDefault(r => r.Id == id);
+            if (_order == null)
+                return NotFound();
+            _order.Username = admin.Username;
+            _order.Password = admin.Password;
+            _order.Mail = admin.Mail;
+            _order.RegisterTime = admin.RegisterTime;
+
+            _context.Update(_order);
+            _context.SaveChanges();
+            return Created($"api/order/{_order.Id}", _order);
+        }
+
+        // DELETE api/<OrderController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var _order = _context.Admins.FirstOrDefault(r => r.Id == id);
+            if (_order == null)
+                return NotFound();
+            _context.Remove(_order);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ Code/Controllers/Controllers/BookController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using E_Book_Trading_Platform.Models;
+
+namespace E_Book_Trading_Platform.Controllers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly Models.PlatformContext _context;
+
+        public BookController(Models.PlatformContext context)
+        {
+            _context = context;
+        }
+
+        // GET:
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Books.ToList());
+        }
+        //GET: 
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var _book = _context.Books.FirstOrDefault(r => r.Id == id);
+            if (_book == null)
+                return NotFound();
+            return Ok(_book);
+        }
+        // POST api/<OrderController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Book book)
+        {
+            _context.Add(book);
+            _context.SaveChanges();
+            return Created($"api/Order/{book.Id}", book);
+        }
+
+        // PUT api/<OrderController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Book book)
+        {
+            var _book = _context.Books.FirstOrDefault(r => r.Id == id);
+            if (_book == null)
+                return NotFound();
+            _book.Author = book.Author;
+            _book.Price = book.Price;
+            _book.Name = book.Name;
+            _book.Image = book.Image;
+            _book.Description = book.Description;
+
+            _context.Update(_book);
+            _context.SaveChanges();
+            return Created($"api/order/{_book.Id}", _book);
+        }
+
+        // DELETE api/<OrderController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var _book = _context.Books.FirstOrDefault(r => r.Id == id);
+            if (_book == null)
+                return NotFound();
+            _context.Remove(_book);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
